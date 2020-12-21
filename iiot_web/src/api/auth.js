f4f63e07de263d6f707bbcc60f2fe3f5da8698c1
Kong Xiaosong <autokxs@163.com>
2020-12-21 22:01:45 +0800

    update url address

--- iiot_web/src/api/auth.js
+++ iiot_web/src/api/auth.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function Signin(data) {
     return request({
-        url: '/api/login/',
+        url: '/login/',
         method: 'post',
         data
     })
