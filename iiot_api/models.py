a8c273f38bae836225044002df6b5c1665c6cdc3
Kong Xiaosong <autokxs@163.com>
2020-12-26 22:22:11 +0800

    add table for device

--- iiot_api/models.py
+++ iiot_api/models.py
@@ -9,3 +9,36 @@ class Book(models.Model):
         return self.book_name
 
 # Create your models here.
+class Device(models.Model):
+    # identifier for records
+    device_id = models.IntegerField()
+    steam_pool_id = models.IntegerField()
+    #status registers
+    auto_run = models.BooleanField()
+    temp_hold = models.BooleanField()
+    valve_mannal = models.BooleanField()
+    sensor_alarm = models.BooleanField()
+    valve_auto = models.BooleanField()
+    running_light = models.BooleanField()
+    # status registers  --- for power off only
+    enable_schedule = models.BooleanField()
+    # word registers
+    temp = models.IntegerField()
+    temp_time_count = models.IntegerField()
+    current_step = models.IntegerField()
+    target_temp = models.IntegerField()
+    target_rate = models.IntegerField()
+    target_time = models.IntegerField()
+    estimated_temp = models.IntegerField()
+    pid_output = models.IntegerField()
+    heating_count = models.IntegerField()
+    ad_temp = models.IntegerField()
+    # word registers  --- for power off only
+    valve_count = models.IntegerField()
+    craft_selection = models.IntegerField()
+
+    # metadata for records
+    add_time = models.DateTimeField(auto_now_add=True)
+    upload_time = models.DateTimeField()
+
+
