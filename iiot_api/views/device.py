aa77986fd67589afaaeba7660880b855548f9af9
Xiaosong <autokxs@163.com>
2020-12-30 05:20:25 +0000

    api for device

--- iiot_api/views/device.py
+++ iiot_api/views/device.py
@@ -2,11 +2,11 @@ from rest_framework import mixins
 from rest_framework import generics
 import sys
 sys.path.append("..")
-from models import Device
-from serializers import DeviceSerializer
+from iiot_api import models
+from iiot_api import serializers
 class DeviceList(mixins.ListModelMixin,generics.GenericAPIView):
-    queryset = Device.objects.all()
-    serializer_class = DeviceSerializer
+    queryset = models.Device.objects.all()
+    serializer_class = serializers.DeviceSerializer
 
     def get(self,request,*args,**kwargs):
         return self.list(request,*args,**kwargs)
