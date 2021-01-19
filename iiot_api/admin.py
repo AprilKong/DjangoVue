a9985c3dff77bec8387007f1e5ad876fced7c8a5
Xiaosong <autokxs@163.com>
2021-01-19 14:09:17 +0000

    admin new user table

--- iiot_api/admin.py
+++ iiot_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import IiotUser
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.register(IiotUser,UserAdmin)
 
 # Register your models here.
